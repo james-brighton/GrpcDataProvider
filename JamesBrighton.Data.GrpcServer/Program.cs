55df1895a62926f74e21f6df95917c1c84807b22
james-brighton <j.brighton@proton.me>
2023-02-27 14:33:20 +0100

    Added support for web and allow all for CORS

--- JamesBrighton.Data.GrpcServer/Program.cs
+++ JamesBrighton.Data.GrpcServer/Program.cs
@@ -1,22 +1,22 @@
 using System.Data.Common;
 using JamesBrighton.Data.GrpcServer.Services;
 using FirebirdSql.Data.FirebirdClient;
+using Microsoft.AspNetCore.ResponseCompression;
 
 const string providerInvariantName = "FirebirdSql.Data.FirebirdClient";
 DbProviderFactories.RegisterFactory(providerInvariantName, FirebirdClientFactory.Instance);
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-
-// Add services to the container.
+builder.Services.AddCors(o => o.AddPolicy("AllowAll", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding")));
 builder.Services.AddGrpc();
 
 var app = builder.Build();
+
 app.UseGrpcWeb();
-// Configure the HTTP request pipeline.
-app.MapGrpcService<DatabaseService>().EnableGrpcWeb();
+app.UseCors();
+
+app.MapGrpcService<DatabaseService>().EnableGrpcWeb().RequireCors("AllowAll");
 app.MapGet("/", () =>"Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
