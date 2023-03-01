a0fcb2d67c77fb4ef64a23ae116d6ee67c068c4f
james-brighton <j.brighton@proton.me>
2023-03-01 15:09:15 +0100

    Updated example

--- JamesBrighton.Data.GrpcTest.SharedClient/Views/MainView.axaml.cs
+++ JamesBrighton.Data.GrpcTest.SharedClient/Views/MainView.axaml.cs
@@ -16,8 +16,55 @@ public partial class MainView : UserControl
 
     async void ButtonClick(object? sender, RoutedEventArgs e)
     {
+        items.Clear();
+        dataGrid.Columns.Clear();
+        outputTextBlock.Text = "";
+
         await using var connection = GrpcClientFactory.Instance.CreateConnection() as IAsyncGrpcConnection;
         if (connection == null) return;
+        SetupConnection(connection);
+        try
+        {
+            await connection.OpenAsync();
+        }
+        catch (Grpc.Core.RpcException e1)
+        {
+            outputTextBlock.Text = e1.Message + "\n" + e1.StackTrace;
+            return;
+        }
+        catch (GrpcDataException e1)
+        {
+            outputTextBlock.Text = e1.Message + "\n" + e1.StackTrace;
+            return;
+        }
+        await using var transaction = await connection.BeginTransactionAsync();
+        await using var command = await connection.CreateCommandAsync();
+        SetupCommand(transaction, command);
+
+        try
+        {
+            await using var reader = await command.ExecuteReaderAsync();
+            var firstTime = true;
+            while (await reader.ReadAsync(CancellationToken.None))
+            {
+                if (firstTime)
+                {
+                    SetupColumns(reader);
+                    firstTime = false;
+                }
+                ReadLine(reader);
+            }
+
+            await transaction.CommitAsync(CancellationToken.None);
+        }
+        catch (GrpcDataException e1)
+        {
+            await ShowException(transaction, e1);
+        }
+    }
+
+    void SetupConnection(IAsyncGrpcConnection? connection)
+    {
         var connectionStringBuilder = GrpcClientFactory.Instance.CreateConnectionStringBuilder();
         connectionStringBuilder["GrpcServer"] = "http://localhost:5056/";
         connection.ConnectionString = connectionStringBuilder.ToString() ?? "";
@@ -28,63 +75,20 @@ public partial class MainView : UserControl
         connectionStringBuilder["Database"] = databaseTextBox.Text ?? "";
         connectionStringBuilder["WireCrypt"] = "Required";
         connection.ServerConnectionString = connectionStringBuilder.ToString() ?? "";
-        try
-        {
-            await connection.OpenAsync();
-        }
-        catch (Grpc.Core.RpcException e1)
-        {
-            outputTextBlock.Text = e1.Message + "\n" + e1.StackTrace;
-            return;
-        }
-        await using var transaction = await connection.BeginTransactionAsync();
-        await using var command = await connection.CreateCommandAsync();
+    }
+
+    static void SetupCommand(IAsyncDbTransaction transaction, IAsyncDbCommand command)
+    {
         command.Transaction = transaction;
         command.CommandText = "SELECT * FROM EMPLOYEE WHERE EMP_NO > @EMP_NO";
         var parameter = command.CreateParameter();
         parameter.ParameterName = "@EMP_NO";
         parameter.Value = 0;
         command.Parameters.Add(parameter);
-
-        try
-        {
-            await using var reader = await command.ExecuteReaderAsync();
-            var firstTime = true;
-            items.Clear();
-            while (await reader.ReadAsync(CancellationToken.None))
-            {
-                if (firstTime)
-                {
-                    SetupColumns(reader);
-                    firstTime = false;
-                }
-                var item = new List<string>();
-                for (var i = 0; i < reader.FieldCount; i++)
-                {
-                    if (!reader.IsDBNull(i))
-                        item.Add(reader[i].ToString() ?? "");
-                    else
-                        item.Add("<Empty>");
-                }
-                items.Add(item);
-            }
-
-            await transaction.CommitAsync(CancellationToken.None);
-        }
-        catch (GrpcDataException e1)
-        {
-            await transaction.RollbackAsync();
-            var list = new List<string>();
-            var s = e1.ClassName + ": " + e1.Message;
-            for (var i = 0; i < e1.GetPropertyCount(); i++)
-                list.Add(e1.GetPropertyName(i) + ": " + e1[i]);
-            outputTextBlock.Text = s + Environment.NewLine + string.Join(Environment.NewLine, list);
-        }
     }
 
     void SetupColumns(IAsyncDataReader reader)
     {
-        dataGrid.Columns.Clear();
         for (var i = 0; i < reader.FieldCount; i++)
         {
             var dataGridTextColumn = new DataGridTextColumn
@@ -95,6 +99,29 @@ public partial class MainView : UserControl
             };
             dataGrid.Columns.Add(dataGridTextColumn);
         }
+    }
+
+    void ReadLine(IAsyncDataReader reader)
+    {
+        var item = new List<string>();
+        for (var i = 0; i < reader.FieldCount; i++)
+        {
+            if (!reader.IsDBNull(i))
+                item.Add(reader[i].ToString() ?? "");
+            else
+                item.Add("<Empty>");
+        }
+        items.Add(item);
+    }
+
+    async Task ShowException(IAsyncDbTransaction transaction, GrpcDataException exception)
+    {
+        await transaction.RollbackAsync();
+        var list = new List<string>();
+        var s = exception.ClassName + ": " + exception.Message;
+        for (var i = 0; i < exception.GetPropertyCount(); i++)
+            list.Add(exception.GetPropertyName(i) + ": " + exception[i]);
+        outputTextBlock.Text = s + Environment.NewLine + string.Join(Environment.NewLine, list);
     }
 
     readonly AvaloniaList<List<string>> items = new();
