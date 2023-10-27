57d3537933b936e897352ce6d372b5434b3fe308
james-brighton <j.brighton@proton.me>
2023-10-27 19:29:30 +0200

    Removed redundant code

--- JamesBrighton.Data.GrpcServer/Program.cs
+++ JamesBrighton.Data.GrpcServer/Program.cs
@@ -1,17 +1,10 @@
 using System.Data.Common;
 using JamesBrighton.Data.GrpcServer.Services;
 using FirebirdSql.Data.FirebirdClient;
-using ProtoBuf.Meta;
-using FirebirdSql.Data.Types;
 
 const string providerInvariantName = "FirebirdSql.Data.FirebirdClient";
 DbProviderFactories.RegisterFactory(providerInvariantName, FirebirdClientFactory.Instance);
 
-RuntimeTypeModel.Default.Add(typeof(FbZonedDateTime), applyDefaultBehaviour: false)
-    .Add(1, nameof(FbZonedDateTime.DateTime))
-    .Add(2, nameof(FbZonedDateTime.TimeZone))
-    .UseConstructor = false;
-    
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(o => o.AddPolicy("AllowAll", x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding")));
